8165db8988b4366480fc62c00271c8b9fbcb4900
Михаил <repulse30@yandex.ru>
2024-05-19 21:19:03 +0300

    fix: Вторая попытка сдачи проекта №11 Было добавлено логирования в обработчик ошибок ErrorHandler

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,54 +1,62 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.*;
 import ru.yandex.practicum.filmorate.model.ErrorResponse;
-
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler
     public ErrorResponse handleNotFound(NotFoundException e) {
+        log.error("Сообщение об ошибке: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler
     public ErrorResponse handleException(Throwable e) {
+        log.error("Сообщение об ошибке: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
     public ErrorResponse handleValidation(ValidationException e) {
+        log.error("Неправильно указан параметр - {} со значением - {}", e.getParameter(), e.getReason());
         return new ErrorResponse("Некорректное значение параметра " + e.getParameter() + " : " + e.getReason());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
     public ErrorResponse handleConditionsNotMet(ConditionsNotMetException e) {
+        log.error("Сообщение об ошибке: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
     public ErrorResponse handleDateTime(DateTimeException e) {
+        log.error("Сообщение об ошибке: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
     public ErrorResponse handleDimensionalViolation(DimensionalViolationException e) {
+        log.error("Сообщение об ошибке: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
     public ErrorResponse handleDuplicateData(DuplicateDataException e) {
+        log.error("Сообщение об ошибке: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 }
